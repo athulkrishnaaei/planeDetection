6362a10261e60d16a58f06f3d6fd0d072395444d
athulkrishnaaei <athulkrishnakdgr@gmail.com>
2024-08-04 13:27:32 +0200

    removed unwanted files

--- build/my_octomap_converter/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/athul/Hiwi/submodules/planeDetection/ros2/src/my_octomap_converter/my_octomap_converter
-# Build directory: /home/athul/Hiwi/submodules/planeDetection/build/my_octomap_converter
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pointcloud_publisher/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/athul/Hiwi/submodules/planeDetection/install/pointcloud_publisher'
